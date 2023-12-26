685199433b1c489fc21c6f10bcc73b87310f1661
Kobi Hari <kobi2294@yahoo.com>
2023-12-26 19:09:36 +0200

    beautiful question presenter

--- ngrx-new/src/app/app.component.ts
+++ ngrx-new/src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { SharedModule } from './shared.module';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { QuestionPresenterComponent } from './components/question-presenter/question-presenter.component';
+import { Question } from './models/question.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [SharedModule, ToolbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [SharedModule, ToolbarComponent, QuestionPresenterComponent],
 })
 export class AppComponent {
-  title = 'ngrx-new';
+  question: Question = {
+    caption: 'What do you get if you mix red with black',
+    answers: ['red', 'green', 'blue', 'black'],
+    correctIndex: 0,
+  };
 }
--- ngrx-new/src/app/shared.module.ts
+++ ngrx-new/src/app/shared.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from "@angular/common";
 import { NgModule, Type } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
 import { MatRippleModule } from "@angular/material/core";
 import { MatDialogModule } from "@angular/material/dialog";
 import { MatIconModule } from "@angular/material/icon";
+import { MatRadioModule } from "@angular/material/radio";
 import { RouterModule } from "@angular/router";
 
 const sharable: Type<any>[] = [
@@ -13,8 +15,10 @@ const sharable: Type<any>[] = [
     MatDialogModule, 
     MatCardModule, 
     MatRippleModule, 
+    MatRadioModule,
     CommonModule, 
     RouterModule, 
+    ReactiveFormsModule
 ]
 
 @NgModule({
