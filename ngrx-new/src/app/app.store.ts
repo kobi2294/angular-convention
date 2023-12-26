f2861cd50a08c5087d90cc62a9abbf8123fd1960
Kobi Hari <kobi2294@yahoo.com>
2023-12-26 21:36:41 +0200

    store now has methods

--- ngrx-new/src/app/app.store.ts
+++ ngrx-new/src/app/app.store.ts
@@ -1,7 +1,8 @@
-import { signalStore, withComputed, withState } from "@ngrx/signals";
+import { patchState, signalStore, withComputed, withMethods, withState } from "@ngrx/signals";
 import { QuizState } from "./models/quiz-state.model";
 import { QUESTIONS } from "./data/questions";
 import { computed } from "@angular/core";
+import { Answer } from "./models/answer.model";
 
 export const QuizStore = signalStore(
     withState<QuizState>({questions: QUESTIONS, answers: []}), 
@@ -13,5 +14,18 @@ export const QuizStore = signalStore(
     })), 
     withComputed(({questions, currentQuestionIndex}) => ({
         currentQuestion: computed(() => questions()[currentQuestionIndex()])
+    })), 
+    withMethods(({...store}) => ({
+        reset() {
+            patchState(store, {answers: []})
+        }, 
+        answerCurrentQuestion(answerIndex: number) {
+            const question = store.currentQuestion();
+            const answer: Answer = {
+                userAnswer: answerIndex, 
+                isCorrect: question.correctIndex === answerIndex
+            }
+            patchState(store, state => ({answers: [...state.answers, answer]}))
+        }
     }))
 );
