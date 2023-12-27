6660ccc3ded0c48994747111f2ea6abf01014ea0
Kobi Hari <kobi2294@yahoo.com>
2023-12-27 19:11:40 +0200

    now added a random generator for color questions

--- ngrx-new/src/app/app.component.ts
+++ ngrx-new/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { QuestionPresenterComponent } from './components/question-presenter/ques
 import { Question } from './models/question.model';
 import { ProgressComponent } from './components/progress/progress.component';
 import { DoneComponent } from './components/done/done.component';
+import { randomColorQuestion } from './services/helpers';
 
 @Component({
   selector: 'app-root',
@@ -14,11 +15,10 @@ import { DoneComponent } from './components/done/done.component';
   imports: [SharedModule, ToolbarComponent, QuestionPresenterComponent, ProgressComponent, DoneComponent],
 })
 export class AppComponent {
-  question: Question = {
-    caption: 'What do you get if you mix red with black',
-    answers: ['red', 'green', 'blue', 'black'],
-    correctIndex: 0,
-  };
+  question: Question = randomColorQuestion();
+
+  constructor() {
+  }
 
 
 }
