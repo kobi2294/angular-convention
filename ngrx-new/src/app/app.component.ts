c9953b57adcb16b0c7059c64d00a2d9ec7ec4dbb
Kobi Hari <kobi2294@yahoo.com>
2023-12-28 16:13:45 +0200

    Added the redux devtools type info

--- ngrx-new/src/app/app.component.ts
+++ ngrx-new/src/app/app.component.ts
@@ -21,12 +21,28 @@ export class AppComponent {
   store = inject(QuizStore);
   question: Question = randomColorQuestion();
 
-  e = effect(() => {
-    console.log('state changed', getState(this.store));
-  });
-
   constructor() {
+    console.log(window.__REDUX_DEVTOOLS_EXTENSION__);
+    if (window.__REDUX_DEVTOOLS_EXTENSION__) {
+      const connection = window.__REDUX_DEVTOOLS_EXTENSION__.connect({name: 'Lets party'});
+      connection.init({x: 20, y: 30});
 
+      const connection2 = window.__REDUX_DEVTOOLS_EXTENSION__.connect({name: 'Lets party 2'});
+      connection2.init({x: 20, y: 30});
+
+      connection.send('Yipppy 1', {x: 30, y: 30});
+      connection.send('Yipppy 2', {x: 40, y: 20});
+      connection.send('Yipppy 3', {x: 50, y: 10});
+
+      const sub = connection.subscribe(msg => console.log('message from devtools', msg));
+      console.log('connection', connection);
+      console.log('subscription', sub);
+
+      setTimeout(() => {
+        console.log('unsubscribing');
+        sub();
+      }, 60000);
+    }
   }
 
 
---
+++ ngrx-new/src/types/redux-messages.d.ts
@@ -0,0 +1,60 @@
+export interface ReduxStartMessage {
+  readonly type: 'START';
+  readonly state: undefined;
+  readonly id: undefined;
+  readonly source: typeof source;
+}
+
+export interface ReduxStopMessage {
+  readonly type: 'STOP';
+  readonly state: undefined;
+  readonly id: undefined;
+  readonly source: typeof source;
+  readonly failed?: boolean;
+}
+
+export interface ReduxJumpToActionPayload {
+  type: 'JUMP_TO_ACTION';
+  actionId: number;
+}
+
+export interface ReduxDispatchMessage {
+  readonly type: 'DISPATCH';
+  readonly payload: ReduxJumpToStatePayload | ReduxJumpToActionPayload;
+  readonly state: string;
+  readonly id: string;
+  readonly source: string;
+}
+
+interface ReduxActionMessage {
+  readonly type: 'ACTION';
+  readonly payload: string | CustomAction;
+  readonly state: string | undefined;
+  readonly id: string;
+  readonly source: string;
+}
+
+export type ReduxMessage =
+  | ReduxDispatchMessage
+  | ReduxStartMessage
+  | ReduxStopMessage
+  | ReduxSerializedActionMessage
+  ;
+
+export type ReduxState = Record<string, unknown>;
+
+export type ReduxActionType = string;
+
+export interface ReduxAction {
+  readonly type: ReduxActionType;
+}
+
+export type ReduxListener = (msg: ReduxMessage) => void;
+export type ReduxUnsubscriber = () => void;
+
+export interface ReduxDevtoolsConnection {
+  send: (action: ReduxActionType, state: ReduxState) => void;
+  init: (state: ReduxState) => void;
+  subscribe: (listener: ReduxListener) => ReduxUnsubscriber;
+}
+
