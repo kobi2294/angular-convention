02b72d017e6827eb2f80971f8c4e97cfeaded988
Kobi Hari <kobi2294@yahoo.com>
2023-12-28 21:07:53 +0200

    devtools completed

--- ngrx-new/src/app/custom-features/devtools.helpers.ts
+++ ngrx-new/src/app/custom-features/devtools.helpers.ts
@@ -3,33 +3,46 @@ import {
     ReduxAction,
   ReduxActionType,
   ReduxDevtoolsConnection,
+  ReduxDevtoolsUnsubscriber,
   ReduxState,
 } from '../../types/redux-messages';
-import { getState } from '@ngrx/signals';
+import { getState, patchState } from '@ngrx/signals';
 
 type Store = StateSignal<ReduxState>;
+type Session = {
+  readonly connection: ReduxDevtoolsConnection, 
+  readonly unsubscriber: ReduxDevtoolsUnsubscriber
+}
 
-export function reduxDevtoolsInit(
-  instanceName: string,
-  store: Store
-): ReduxDevtoolsConnection | null {
+export function reduxDevtoolsInit(instanceName: string, store: Store): Session | null {
   if (!window.__REDUX_DEVTOOLS_EXTENSION__) return null;
   const connection = window.__REDUX_DEVTOOLS_EXTENSION__.connect({
     name: instanceName,
   });
   const state = getState(store);
   connection.init(state);
-  return connection;
+  const unsubscriber = connection.subscribe(msg => {
+    if ((msg.type === 'DISPATCH') && (msg.payload.type === 'JUMP_TO_ACTION')) {
+      const state = JSON.parse(msg.state);
+      patchState(store, state);
+      console.log(msg);
+    }else {
+    }
+  })
+
+  return {connection, unsubscriber} ;
 }
 
 export function reduxDevtoolsSend(
-  connection: ReduxDevtoolsConnection | null,
+  session: Session | null,
   action: ReduxAction,
   store: Store
 ) {
-  if (!connection) return;
+  if (!session) return;
   const state = getState(store);
-  connection.send(action, state);
+  session.connection.send(action, state);
 }
 
-export function reduxDevtoolsDestroy(store: Store): void {}
+export function reduxDevtoolsDestroy(session: Session | null): void {
+  session?.unsubscriber();
+}
--- ngrx-new/src/app/custom-features/with-devtools.feature.ts
+++ ngrx-new/src/app/custom-features/with-devtools.feature.ts
@@ -1,7 +1,7 @@
 import { PartialStateUpdater, patchState, signalStoreFeature, withHooks, withMethods } from "@ngrx/signals";
 import { StateSignal } from '@ngrx/signals/src/state-signal';
 import { EmptyFeatureResult, SignalStoreFeature, SignalStoreFeatureResult } from '@ngrx/signals/src/signal-store-models';
-import { reduxDevtoolsInit, reduxDevtoolsSend } from './devtools.helpers';
+import { reduxDevtoolsDestroy, reduxDevtoolsInit, reduxDevtoolsSend } from './devtools.helpers';
 import { ReduxAction } from "../../types/redux-messages";
 
 type Updater<Input extends SignalStoreFeatureResult> = Partial<Input['state'] & {}> | PartialStateUpdater<Input['state'] & {}>;
@@ -10,24 +10,18 @@ export function withDevtools<Input extends SignalStoreFeatureResult>(instanceNam
     SignalStoreFeature<Input, EmptyFeatureResult & {methods: { update: <T extends ReduxAction>(action: T, updaters: Updater<Input>) => void}}> {
 
     return store => {
-        const connection = reduxDevtoolsInit(instanceName, store);
-        const subscription = connection!.subscribe(msg => {
-            if ((msg.type === 'DISPATCH') && (msg.payload.type === 'JUMP_TO_ACTION')) {
-                const state = JSON.parse(msg.state) as Input['state'];
-                patchState(store, state);
-            }
-        });
+        const session = reduxDevtoolsInit(instanceName, store);
 
         const feature = signalStoreFeature(
             withMethods((store: StateSignal<Input['state']>) => ({
                 update<T extends ReduxAction>(action: T, updaters: Updater<Input>) {
                     patchState(store, updaters);
-                    reduxDevtoolsSend(connection, action, store);
+                    reduxDevtoolsSend(session, action, store);
                 }
             })), 
             withHooks({
                 onDestroy() {
-                    subscription?.();
+                    reduxDevtoolsDestroy(session);
                 }
             })
         );
