a4b4523a52d4aba4a43479d12e95a1c1523befcb
Kobi Hari <kobi2294@yahoo.com>
2024-01-03 15:16:31 +0200

    removed ngnew 17

--- ng-new-17/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './00-home/home/home.component';
-import { DeferDemoComponent } from './01-defer/defer-demo/defer-demo.component';
-
-export const routes: Routes = [
-    {path:'', redirectTo: 'home', pathMatch: 'full'}, 
-    {path:'home', component: HomeComponent}, 
-    {path:'defer', component: DeferDemoComponent}
-];
