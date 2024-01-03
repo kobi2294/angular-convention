e8661deb8f448f75d59c4be62fbee47d403fe7a3
Kobi Hari <kobi2294@yahoo.com>
2024-01-03 15:20:03 +0200

    renaming folders

---
+++ ng-new-17/src/app/01-defer/defer-demo/defer-demo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-defer-demo',
+  standalone: true,
+  imports: [],
+  templateUrl: './defer-demo.component.html',
+  styleUrl: './defer-demo.component.scss'
+})
+export class DeferDemoComponent {
+
+}
